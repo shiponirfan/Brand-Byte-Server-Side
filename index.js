cd5f5411580901f5b589c4981df962292d2cb606
shiponirfan <imshiponirfan@gmail.com>
2023-10-19 17:42:21 +0600

    Product Details API Created On MongoDB

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
-require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -9,14 +9,14 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@ac-ujyuzy1-shard-00-00.pzomx9u.mongodb.net:27017,ac-ujyuzy1-shard-00-01.pzomx9u.mongodb.net:27017,ac-ujyuzy1-shard-00-02.pzomx9u.mongodb.net:27017/?ssl=true&replicaSet=atlas-lf5h1l-shard-0&authSource=admin&retryWrites=true&w=majority`
+const uri = `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@ac-ujyuzy1-shard-00-00.pzomx9u.mongodb.net:27017,ac-ujyuzy1-shard-00-01.pzomx9u.mongodb.net:27017,ac-ujyuzy1-shard-00-02.pzomx9u.mongodb.net:27017/?ssl=true&replicaSet=atlas-lf5h1l-shard-0&authSource=admin&retryWrites=true&w=majority`;
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -24,40 +24,48 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-    const brandsCollection = client.db("brandsDB").collection("brands")
-    const productsCollection = client.db("brandsDB").collection("products")
+    const brandsCollection = client.db("brandsDB").collection("brands");
+    const productsCollection = client.db("brandsDB").collection("products");
 
-    app.get('/brands', async(req, res) =>{
-        const query = {tag: 'brands'};
-        const brands = await brandsCollection.find(query).toArray();
-        res.send(brands);
+    app.get("/brands", async (req, res) => {
+      const query = { tag: "brands" };
+      const brands = await brandsCollection.find(query).toArray();
+      res.send(brands);
     });
 
-    app.get('/products/:brand_name', async(req, res) =>{
-        const brand_name = req.params.brand_name;
-        const query = {brandName: brand_name};
-        const result = await productsCollection.find(query).toArray();
-        res.send(result);
+    app.get("/products/:brand_name", async (req, res) => {
+      const brand_name = req.params.brand_name;
+      const query = { brandName: brand_name };
+      const result = await productsCollection.find(query).toArray();
+      res.send(result);
     });
 
-    app.post('/products', async(req, res) =>{
-        const product = req.body;
-        const result = await productsCollection.insertOne(product);
-        res.send(result);
+    app.get("/product/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await productsCollection.findOne(query);
+      res.send(result);
+    });
+
+    app.post("/products", async (req, res) => {
+      const product = req.body;
+      const result = await productsCollection.insertOne(product);
+      res.send(result);
     });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
-
   }
 }
 run().catch(console.dir);
 
-app.get('/', (req, res)=>{
-    res.send('Brand Shop Server Side Running');
+app.get("/", (req, res) => {
+  res.send("Brand Shop Server Side Running");
 });
-app.listen(port, ()=>{
-    console.log(`Port Running On: ${port}`);
+app.listen(port, () => {
+  console.log(`Port Running On: ${port}`);
 });
