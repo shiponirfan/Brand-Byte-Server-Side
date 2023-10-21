5ca1ff505731fd11798b10f3a7ccee3823757106
shiponirfan <imshiponirfan@gmail.com>
2023-10-21 22:40:26 +0600

    Product Update Issue Fixed

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(express.json());
 const corsConfig = {
   origin: "*",
   credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
 };
 app.use(cors(corsConfig));
 
